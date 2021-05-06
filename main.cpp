2cb70d7f4da3725a1f16faad4b51c64e715ca4fc
Kevin Pham <67347904+phamkevin04@users.noreply.github.com>
2021-05-05 17:51:03 -0700

    test array
    tested arrays for letters and morse code to make sure they work and hold data as expected

--- main.cpp
+++ main.cpp
@@ -14,9 +14,25 @@ int main() {
     return 0;
 }
 
+
 // set the letters of the alphabet for reference
 void setLetters() { 
     string letters[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "};
-    cout << letters[2] << letters[0] << letters[1] << endl; // result: cab
-    cout << letters[3] << letters[0] << letters[1] << endl; // result: dab
+    // cout << letters[2] << letters[0] << letters[1] << endl; // result: cab
+    
+    string morses[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "   "};
+    // cout << morses[3] << " / " << morses[0] << " / " << morses[1] << endl; // result: dab
+
+    // test to make sure that the letters and morse alphabet is correct
+    int len = sizeof(letters)/sizeof(letters[0]);
+    
+    for(int i = 0; i < len; i++) { // test letters
+        cout << letters[i] << " ";
+    }
+    cout << endl;
+
+        for(int i = 0; i < len; i++) { // test morse
+        cout << morses[i] << " ";
+    }
+    cout << endl;
 }
